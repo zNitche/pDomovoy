04f429518386e3513becf604986e0ffd6e9bd80c
zNitche <53448802+zNitche@users.noreply.github.com>
2026-01-23 21:55:29 +0100

    build: project cmake related improvements

--- cmake/client.cmake
+++ cmake/client.cmake
@@ -5,8 +5,3 @@ target_link_libraries(client pico_stdlib)
 if (PICO_CYW43_SUPPORTED)
     target_link_libraries(client pico_cyw43_arch_none)
 endif()
-
-pico_enable_stdio_usb(client 1)
-pico_enable_stdio_uart(client 0)
-
-pico_add_extra_outputs(client)
--- cmake/server.cmake
+++ cmake/server.cmake
@@ -5,8 +5,3 @@ target_link_libraries(server pico_stdlib)
 if (PICO_CYW43_SUPPORTED)
     target_link_libraries(server pico_cyw43_arch_none)
 endif()
-
-pico_enable_stdio_usb(server 1)
-pico_enable_stdio_uart(server 0)
-
-pico_add_extra_outputs(server)
