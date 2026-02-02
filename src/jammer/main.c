4c533ed14c09194cd2e73bccb6fb9fbe088d38bd
zNitche <53448802+zNitche@users.noreply.github.com>
2026-02-02 22:57:50 +0100

    feat: enabled adxl345 for jammer

--- src/jammer/main.c
+++ src/jammer/main.c
@@ -98,49 +98,45 @@ int main() {
   gpio_init(ACTION_BUTTON_PIN);
   gpio_set_dir(ACTION_BUTTON_PIN, GPIO_IN);
   gpio_pull_down(ACTION_BUTTON_PIN);
-  gpio_set_irq_enabled_with_callback(ACTION_BUTTON_PIN,
-                                     GPIO_IRQ_EDGE_RISE,
+  gpio_set_irq_enabled_with_callback(ACTION_BUTTON_PIN, GPIO_IRQ_EDGE_RISE,
                                      true, &button_callback);
 
   // LED
   gpio_init(STATUS_LED_PIN);
   gpio_set_dir(STATUS_LED_PIN, true);
 
+  // ADXL345
+  ADXL345I2C adxl345_i2c = {i2c0, 0x53, 0, 1};
+
+  adxl345_setup_i2c(adxl345_i2c);
+  sleep_ms(250);
+
+  bool connected = adxl345_check_connection(adxl345_i2c);
+
+  printf("connected: %d\n", connected);
+
+  enum ADXL345Ranges sensor_range = RANGE_4G;
+  adxl345_set_measurements_range(adxl345_i2c, sensor_range);
+
+  adxl345_start_measurements(adxl345_i2c);
+
   while (true) {
     gpio_put(STATUS_LED_PIN, true);
     sleep_ms(250);
     gpio_put(STATUS_LED_PIN, false);
     sleep_ms(250);
+
+    if (connected) {
+      float accel[3] = {0.0};
+      adxl345_get_readings(adxl345_i2c, accel);
+
+      printf("Acc. X = %f, Y = %f, Z = %f\n", accel[0], accel[1], accel[2]);
+
+      sleep_ms(200);
+    }
   }
 
-  // ADXL345I2C adxl345_i2c = {i2c0, 0x53, 0, 1};
-
-  // adxl345_setup_i2c(adxl345_i2c);
-  // sleep_ms(250);
-
-  // bool connected = adxl345_check_connection(adxl345_i2c);
-
-  // printf("connected: %d\n", connected);
-
-  // enum ADXL345Ranges sensor_range = RANGE_4G;
-  // adxl345_set_measurements_range(adxl345_i2c, sensor_range);
-
-  // adxl345_start_measurements(adxl345_i2c);
-
-  // if (connected == true) {
-  //   printf("processing...\n");
-  //   float accel[3] = {0.0};
-
-  //   while (true) {
-  //     adxl345_get_readings(adxl345_i2c, accel);
-
-  //     printf("Acc. X = %f, Y = %f, Z = %f\n", accel[0], accel[1], accel[2]);
-
-  //     sleep_ms(200);
-  //   }
-
-  //   adxl345_stop_measurements(adxl345_i2c);
-  // }
+  adxl345_stop_measurements(adxl345_i2c);
 
   return 0;
 }
