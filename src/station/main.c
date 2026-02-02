e3016e59da638a57626ec93ab8751759daadde67
zNitche <53448802+zNitche@users.noreply.github.com>
2026-02-02 22:40:40 +0100

    fix: proper directory of station -> main.c

---
+++ src/station/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "pico/cyw43_arch.h"
+
+int main()
+{
+    stdio_init_all();
+
+    int rc = cyw43_arch_init();
+    hard_assert(rc == PICO_OK);
+
+    while (true)
+    {
+        printf("station\n");
+        sleep_ms(1000);
+    }
+}
