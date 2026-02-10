d330e0103202e9a011586b12e80c5961878a97bc
zNitche <53448802+zNitche@users.noreply.github.com>
2026-02-10 18:49:06 +0100

    feat: removed disabling alarm in standby mode

--- modules/jammer/src/handlers.c
+++ modules/jammer/src/handlers.c
@@ -7,11 +7,8 @@
 void toggle_alarm_standby() {
     if (!g_alarm_in_standby) {
         multicore_launch_core1(core_1);
-    } else {
-        multicore_reset_core1();
+        g_alarm_in_standby = true;
     }
-
-    g_alarm_in_standby = !g_alarm_in_standby;
 }
 
 void disable_triggered_alarm() {
