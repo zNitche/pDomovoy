a26f36b22147deaa8fec0c3053bc5997d49407a4
zNitche <53448802+zNitche@users.noreply.github.com>
2026-02-11 18:58:58 +0000

    feat: led_blink.c - blink_untill_start() - added init guard

--- modules/jammer/src/utils/led_blink.c
+++ modules/jammer/src/utils/led_blink.c
@@ -20,7 +20,10 @@ void blink_blocking(int pin, int times, int time_between) {
 
 void blink_untill_start(int pin, int32_t time_between,
                         repeating_timer_callback_t cb) {
-    add_repeating_timer_ms(time_between, cb, NULL, &g_status_led_blink_timer);
+    if (!g_status_led_blink_timer.alarm_id) {
+        add_repeating_timer_ms(time_between, cb, NULL,
+                               &g_status_led_blink_timer);
+    }
 }
 
 void blink_untill_stop(int pin) {
