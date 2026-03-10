491d98e67f29ddb5a0d49c9a33a5287fef207701
zNitche <53448802+zNitche@users.noreply.github.com>
2026-03-10 19:48:16 +0100

    refactor: warden - changed acceleration readings for alarm trigger check

--- modules/warden/src/cores/core_1.c
+++ modules/warden/src/cores/core_1.c
@@ -41,7 +41,7 @@ bool _check_for_trigger_for_axis(float initial_mean, float mean,
 
 bool _check_for_alarm_trigger(ADXL345I2C* adxl345_i2c,
                               float initial_accel_mean[3]) {
-    const int readings_count = 5;
+    const int readings_count = 10;
     const float trigger_factor = 0.2;
 
     float total_accel[readings_count][3] = {};
