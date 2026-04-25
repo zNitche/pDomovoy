e32e1111f7585984add614abf287f6b00cd643d1
zNitche <53448802+zNitche@users.noreply.github.com>
2026-04-25 23:53:17 +0200

    feat: trumpet - gatt events logging

--- modules/trumpet/src/bluetooth.c
+++ modules/trumpet/src/bluetooth.c
@@ -1,5 +1,6 @@
 #include "../includes/bluetooth.h"
 
+#include "test_gatt.h"
 #include "../includes/globals.h"
 #include "btstack.h"
 #include "pdomovoy_common/debug_print.h"
@@ -26,8 +27,10 @@ void __setup_gap_advertisements() {
     uint16_t adv_int_min = 800;
     uint16_t adv_int_max = 800;
     uint8_t adv_type = 0;
+
     bd_addr_t null_addr;
     memset(null_addr, 0, 6);
+
     gap_advertisements_set_params(adv_int_min, adv_int_max, adv_type, 0,
                                   null_addr, 0x07, 0x00);
     gap_advertisements_set_data(sizeof(adv_data), (uint8_t*)adv_data);
@@ -39,19 +42,15 @@ static void __packet_handler(uint8_t packet_type, uint16_t channel,
     UNUSED(size);
     UNUSED(channel);
     bd_addr_t local_addr;
-
     if (packet_type != HCI_EVENT_PACKET)
         return;
 
     uint8_t event_type = hci_event_packet_get_type(packet);
 
-    debug_print("[BTstack] event type: %d\n", event_type);
-
     switch (event_type) {
     case BTSTACK_EVENT_STATE:
-        if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) {
+        if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING)
             return;
-        }
 
         gap_local_bd_addr(local_addr);
         debug_print("[BTstack] up and running on %s.\n",
@@ -59,6 +58,13 @@ static void __packet_handler(uint8_t packet_type, uint16_t channel,
 
         __setup_gap_advertisements();
 
+        break;
+    case HCI_EVENT_LE_META:
+        switch (hci_event_le_meta_get_subevent_code(packet)) {
+        case HCI_SUBEVENT_LE_CONNECTION_COMPLETE:
+            debug_print("client connected \n");
+            break;
+        }
         break;
     case HCI_EVENT_CONNECTION_REQUEST:
         debug_print("conection request\n");
@@ -69,8 +75,6 @@ static void __packet_handler(uint8_t packet_type, uint16_t channel,
     case HCI_EVENT_DISCONNECTION_COMPLETE:
         debug_print("disconnected\n");
         break;
-    case ATT_EVENT_CAN_SEND_NOW:
-        break;
     default:
         break;
     }
@@ -80,7 +84,7 @@ void init_ble() {
     l2cap_init();
     sm_init();
 
-    att_server_init(g_btstack_profile_data, NULL, NULL);
+    att_server_init(profile_data, NULL, NULL);
 
     gatt_client_set_required_security_level(LEVEL_2);
     sm_set_authentication_requirements(SM_AUTHREQ_BONDING |
