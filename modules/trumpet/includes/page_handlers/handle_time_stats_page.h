6b38be1b5a3633956aa2f031b77a7c690af76552
zNitche <53448802+zNitche@users.noreply.github.com>
2026-05-06 21:22:18 +0000

    feat: trumpet - time stats page - alarm armed time integration

--- modules/trumpet/includes/page_handlers/handle_time_stats_page.h
+++ modules/trumpet/includes/page_handlers/handle_time_stats_page.h
@@ -9,5 +9,6 @@ void __timestamp_to_hhmm(uint32_t timestamp, uint32_t* hours,
 
 void __get_boot_time_data(uint32_t* hours, uint32_t* minutes);
 void __get_warden_connection_time_data(uint32_t* hours, uint32_t* minutes);
+void __get_g_alarm_armed_time_data(uint32_t* hours, uint32_t* minutes);
 
 void handle_time_stats_page(SSD1306_Frame* frame);
--- modules/trumpet/src/page_handlers/handle_time_stats_page.c
+++ modules/trumpet/src/page_handlers/handle_time_stats_page.c
@@ -45,18 +45,37 @@ void __get_warden_connection_time_data(uint32_t* hours, uint32_t* minutes) {
     __timestamp_to_hhmm(timestamp, hours, minutes);
 }
 
+void __get_g_alarm_armed_time_data(uint32_t* hours, uint32_t* minutes) {
+    static const uint32_t interval_ms = 5000;
+    static uint32_t next_runtime = 0;
+
+    if (!should_execute_repeating_function(&next_runtime, interval_ms)) {
+        return;
+    }
+
+    const uint32_t timestamp =
+        to_ms_since_boot(get_absolute_time()) - g_alarm_armed_timestamp;
+
+    __timestamp_to_hhmm(timestamp, hours, minutes);
+}
+
 void handle_time_stats_page(SSD1306_Frame* frame) {
     static uint32_t hours_since_boot = 0;
     static uint32_t minutes_since_boot = 0;
     static uint32_t hours_since_warden_connected = 0;
     static uint32_t minutes_since_warden_connected = 0;
+    static uint32_t hours_since_alarm_armed = 0;
+    static uint32_t minutes_since_alarm_armed = 0;
 
     static char time_data_since_boot_str[15];
     static char time_data_since_warden_connected_str[13];
+    static char time_data_since_alarm_armed_str[13];
 
     __get_boot_time_data(&hours_since_boot, &minutes_since_boot);
     __get_warden_connection_time_data(&hours_since_warden_connected,
                                       &minutes_since_warden_connected);
+    __get_g_alarm_armed_time_data(&hours_since_alarm_armed,
+                                  &minutes_since_alarm_armed);
 
     snprintf(time_data_since_boot_str, sizeof(time_data_since_boot_str),
              "T booted %u:%u", hours_since_boot, minutes_since_boot);
@@ -70,5 +89,14 @@ void handle_time_stats_page(SSD1306_Frame* frame) {
 
         ssd1306_render_string(frame, 0, 28,
                               time_data_since_warden_connected_str, 1, false);
+
+        if (g_alarm_armed_timestamp != 0) {
+            snprintf(time_data_since_alarm_armed_str,
+                     sizeof(time_data_since_alarm_armed_str), "Armed %u:%u",
+                     hours_since_alarm_armed, minutes_since_alarm_armed);
+
+            ssd1306_render_string(frame, 0, 40, time_data_since_alarm_armed_str,
+                                  1, false);
+        }
     }
 }
